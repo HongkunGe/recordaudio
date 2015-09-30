2319ed02778035d42ee79a51bc281039f4c02a71
HongkunGe <hongkun@cs.unc.edu>
2015-09-30 09:24:13 -0400

    add recordwrodker.js

--- server.js
+++ server.js
@@ -112,18 +112,13 @@ var SampleApp = function() {
      *  the handlers.
      */
     self.initializeServer = function() {
-        
-
         self.createRoutes();
         self.app = express.createServer();
-        // 
+        // self.app.use(express.static(__dirname));
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
             self.app.get(r, self.routes[r]);
         }
-        ['css', 'img', 'js', 'plugin', 'lib'].forEach(function (dir){
-            self.app.use('/'+dir, express.static(__dirname+'/'+dir));
-        });
     };
 
 
