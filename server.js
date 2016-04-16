8ba1f2d895dbb10235989bf582a3ac9f816b6904
HongkunGe <hongkun@cs.unc.edu>
2016-04-16 17:42:02 -0400

    enable trust proxy and implement feature of redirection

--- server.js
+++ server.js
@@ -126,6 +126,12 @@ var SampleApp = function() {
         self.createRoutes();
         self.app = express();
 
+        // Enable reverse proxy support in Express. This causes the
+        // the "X-Forwarded-Proto" header field to be trusted so its
+        // value can be used to determine the protocol. See
+        // http://expressjs.com/api#app-settings for more details.
+        self.app.enable('trust proxy');
+
         // Add a handler to inspect the req.secure flag (see
         // http://expressjs.com/api#req.secure). This allows us
         // to know whether the request was via http or https.
@@ -137,8 +143,6 @@ var SampleApp = function() {
                         // request was via http, so redirect to https
                         res.redirect('https://' + req.headers.host + req.url);
                 }
-                console.log(req.headers.host);
-                console.log(req);
         });
 
         // self.app.use(express.static(__dirname));
